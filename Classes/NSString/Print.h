28d964b687893e045b4b0fe6ff1137796905ff16
malagu <liujiajun888666@icloud.com>
2019-11-15 16:54:14 +0800

    add print file

---
+++ Classes/NSString/Print.h
@@ -0,0 +1,17 @@
+//
+//  Print.h
+//  TestDevice
+//
+//  Created by jiajun liu on 2019/11/15.
+//  Copyright © 2019 jiajun liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Print : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/NSString/Print.m
@@ -0,0 +1,13 @@
+//
+//  Print.m
+//  TestDevice
+//
+//  Created by jiajun liu on 2019/11/15.
+//  Copyright © 2019 jiajun liu. All rights reserved.
+//
+
+#import "Print.h"
+
+@implementation Print
+
+@end
